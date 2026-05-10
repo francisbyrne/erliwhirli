787b48ca02a39267f4a3dabac4bf4c5689b8847e
francisbyrne <byrnify@gmail.com>
2026-05-10 13:52:43 +1000

    update app script to new deployment

--- settings.js
+++ settings.js
@@ -2,5 +2,5 @@ window.CROSSWORD_SETTINGS = {
   sheetId: "1WnNEgj1pwetAm6Cu2ZCyIJfAoldQFqOiTPHb914qkhs",
   sheetGid: "0",
   leaderboardPostUrl:
-    "https://script.google.com/macros/s/AKfycbzwkKqwefqy_Q2YIszlqPRLK2nLDKucWwk6pyKv1W_lEWDPLCF_rz8vceFEOYydSdFKTg/exec",
+    "https://script.google.com/macros/s/AKfycbx_q9qjeMzP-PdLxb0mIB2InjvIA3ZpPjTOj8dLWFUcLetK4tO4kZBHWS43wbAnoHdMoA/exec",
 };
