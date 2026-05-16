a89404532404c29265befd7d885d2dcd20641733
francisbyrne <byrnify@gmail.com>
2026-05-16 17:30:18 +1000

    move settings into index.html
    so claude preview works

--- settings.js
+++
@@ -1,7 +0,0 @@
-window.CROSSWORD_SETTINGS = {
-  sheetId: "1WnNEgj1pwetAm6Cu2ZCyIJfAoldQFqOiTPHb914qkhs",
-  sheetGid: "0",
-  leaderboardGid: "1934708003",
-  leaderboardPostUrl:
-    "https://script.google.com/macros/s/AKfycbx_q9qjeMzP-PdLxb0mIB2InjvIA3ZpPjTOj8dLWFUcLetK4tO4kZBHWS43wbAnoHdMoA/exec",
-};
